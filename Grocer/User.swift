1eeb0a91ce1884fd4c5ebb117a532e977535e368
richardgmartin <richardgeorgemartin@gmail.com>
2016-11-06 10:45:36 -0500

    Added User and GroceryItem models (structs).

---
+++ Grocer/User.swift
@@ -0,0 +1,26 @@
+//
+//  User.swift
+//  Grocer
+//
+//  Created by Richard Martin on 2016-11-06.
+//  Copyright © 2016 richard martin. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+struct User {
+    
+    let uid: String
+    let email: String
+    
+    init(authData: FIRUser) {
+        uid = authData.uid
+        email = authData.email!
+    }
+    
+    init(uid: String, email: String) {
+        self.uid = uid
+        self.email = email
+    }
+}
